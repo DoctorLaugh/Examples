a8821c3e0913b4da7c105910ffdd89f75eb8ddd3
Dmitriy <trayan@yandex.ru>
2023-06-19 13:40:24 +1100

    If/Else added

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите Ваше имя");
+string name = Console.ReadLine();
+if(name == "Маша")
+{
+    Console.WriteLine("РСЦ ин да хаус");
+}
+else
+{
+    Console.WriteLine("Кто здесь?");
+}
